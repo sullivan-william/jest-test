e482576691473f86465fc106341c1faebdb0a46f
William Sullivan <sullivanwp95@gmail.com>
2023-04-29 15:26:29 -0400

    Adding second test

--- capitalize.test.js
+++ capitalize.test.js
@@ -3,3 +3,7 @@ const capitalize = require('./capitalize')
 test('hello to return Hello', () => {
     expect(capitalize('hello')).toBe('Hello')
 })
+
+test('Already capitalized', () => {
+    expect(capitalize('Hello')).toBe('Hello')
+})
